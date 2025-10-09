6a82c61440eb1e0906881d1aa30a0c30b43be2c2
MANISH KUMAR AGRAHARI <mka9146866@gmail.com>
2025-10-09 22:51:08 +0530

    Add files via upload

---
+++ rani.py
@@ -0,0 +1,22 @@
+# # # str1="hello allterbhaiiiii"
+# # # str2="welcome"
+# # # print(str1[0:2])
+# # # print(str2[1:3])
+# # # print(str2[4])
+# # # print(str2[5])
+# # # print(str2+str1)
+# # # print(str1+str2)
+
+# # a = True
+# # print(a)
+# # print(type(a))
+
+
+# type casting
+#  
+# s="10010"
+# c=int(s,2)
+# print(c)
+# e=float(c)
+# print(e)
+
